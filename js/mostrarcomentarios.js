63ac1bb6906597ad11c61daee9f6b954bca3b3ec
Marc <marc@prueba.com>
2024-01-23 19:09:41 +0100

    Mostrar comentarios con diseño terminado

--- js/mostrarcomentarios.js
+++ js/mostrarcomentarios.js
@@ -13,10 +13,11 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log(pepe);
         let seccion = document.getElementById('contenedor');
         pepe.forEach(comentario => {
-            let div = document.createElement("div");
+            let div = document.createElement("article");
             div.className="contenedorlogin col-10 mb-4"
             seccion.appendChild(div);
             let nombre = document.createElement('h3');
+            nombre.className="negrita"
             let valoracionElemento = document.createElement('p');
             let comentariosElemento = document.createElement('p');
             nombre.innerHTML=comentario["Nombre_Usuario"];
@@ -24,7 +25,23 @@ document.addEventListener('DOMContentLoaded', function() {
             comentariosElemento.innerHTML=comentario["Comentario"];
             div.appendChild(nombre);
             div.appendChild(comentariosElemento);
-            div.appendChild(valoracionElemento);
+            let imagenes = document.createElement("div");
+            div.appendChild(imagenes);
+            for (let i = 0; i < comentario["Valoracion"]; i++) {
+                let valoracionElemento = document.createElement('img');
+                valoracionElemento.src = "Imagenes/Iconos/estrellarellena.svg";
+                valoracionElemento.className="estrella"
+                imagenes.appendChild(valoracionElemento);
+            }
+            let estrellavacia = 5-comentario["Valoracion"];
+            for (let i = 0; i < estrellavacia; i++) {
+                let valoracionElemento = document.createElement('img');
+                valoracionElemento.src = "Imagenes/Iconos/estrella.svg";
+                valoracionElemento.className="estrella"
+                imagenes.appendChild(valoracionElemento);
+            }
+
+            
         });
     })
 });
