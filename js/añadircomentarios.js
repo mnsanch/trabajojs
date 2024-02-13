6e6fca000bfd0bfb177dac8c5c20a56d98820886
Marc <marc@prueba.com>
2024-02-13 19:00:26 +0100

    Noty en formulario añadir comentarios y mejora en las estrellas del formulario

--- js/añadircomentarios.js
+++ js/añadircomentarios.js
@@ -3,6 +3,20 @@ document.getElementById("botonaceptar").addEventListener("click", function () {
     const nombre = document.getElementById("nombre").value;
     const comentario = document.getElementById("comentario").value;
     const valoracion = document.querySelector('input[name="puntuacion"]:checked').value;
+
+    
+        // Validaremos que todos los campos estén completos antes de procesar el comentario:
+        if (!nombre || !comentario || !valoracion) {
+       
+          // Mostraremos un mensaje de error en el caso de que no complete todos los campos del formulario:
+          notie.alert({
+              type: 'error',
+              text: 'Completa todos los campos antes de agregar el comentario',
+              time: 3
+          });
+          return;
+      }
+
     
     // Crear objeto con los datos del formulario
     const data = {
@@ -10,9 +24,6 @@ document.getElementById("botonaceptar").addEventListener("click", function () {
       comentario: comentario,
       valoracion: valoracion 
     };
-    console.log(data)
-    console.log(nombre)
-    console.log(data.nombre)
 
     // Enviar los datos al servidor
     fetch('http://localhost/trabajojs/index.php?controller=API&action=añadircomentarios', {
@@ -21,5 +32,11 @@ document.getElementById("botonaceptar").addEventListener("click", function () {
             'Content-type': 'application/json; charset=UTF-8',
         },
         body: JSON.stringify(data)
+    });
+
+    notie.alert({
+        type: 'success',
+        text: 'Comentario agregado exitosamente',
+        time: 3
     });
   });
